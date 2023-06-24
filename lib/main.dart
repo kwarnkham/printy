04a1b976ac00d109dcb221fa930778b3df1575ce
Moon <kwarnkham.moon@gmail.com>
2023-06-24 11:28:41 +0630

    #

--- lib/main.dart
+++ lib/main.dart
@@ -32,13 +32,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late BluetoothDevice _device;
-
+  BluetoothDevice? _device;
+  bool _scanning = false;
+  bool _connecting = false;
+  bool _connected = false;
   final BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
 
+  _MyHomePageState() {
+    bluetoothPrint.state.listen((state) {
+      switch (state) {
+        case BluetoothPrint.CONNECTED:
+          setState(() {
+            _connected = true;
+          });
+          break;
+        case BluetoothPrint.DISCONNECTED:
+          setState(() {
+            _connected = false;
+          });
+          break;
+        default:
+          break;
+      }
+    });
+  }
   void _scanForBluetoothDevices() {
-    print('sacnning');
-    bluetoothPrint.startScan(timeout: const Duration(seconds: 4));
+    setState(() {
+      _scanning = true;
+    });
+
+    bluetoothPrint
+        .startScan(timeout: const Duration(seconds: 4))
+        .then((value) => {
+              setState(() {
+                _scanning = false;
+              })
+            });
+  }
+
+  void _connectDevice() async {
+    setState(() {
+      _connecting = true;
+    });
+    try {
+      await bluetoothPrint.connect(_device!);
+    } catch (e) {
+      rethrow;
+    }
+    setState(() {
+      _connecting = false;
+    });
+  }
+
+  void _print() async {
+    Map<String, dynamic> config = {};
+    List<LineText> list = List.empty();
+    list.add(LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'A Title',
+        weight: 1,
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1));
+    list.add(LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'this is conent left',
+        weight: 0,
+        align: LineText.ALIGN_LEFT,
+        linefeed: 1));
+    list.add(LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'this is conent right',
+        align: LineText.ALIGN_RIGHT,
+        linefeed: 1));
+    list.add(LineText(linefeed: 1));
+    list.add(LineText(
+        type: LineText.TYPE_BARCODE,
+        content: 'A12312112',
+        size: 10,
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1));
+    list.add(LineText(linefeed: 1));
+    list.add(LineText(
+        type: LineText.TYPE_QRCODE,
+        content: 'qrcode i',
+        size: 10,
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1));
+    list.add(LineText(linefeed: 1));
+
+    await bluetoothPrint.printReceipt(config, list);
   }
 
   @override
@@ -52,6 +134,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+                onPressed: (_connected) ? _print : null,
+                child: const Text('Print')),
+            ElevatedButton(
+                onPressed:
+                    (_device != null && !_connecting) ? _connectDevice : null,
+                child: const Text('Connect')),
             StreamBuilder<List<BluetoothDevice>>(
               stream: bluetoothPrint.scanResults,
               initialData: const [],
@@ -65,21 +154,22 @@ class _MyHomePageState extends State<MyHomePage> {
                               _device = d;
                             });
                           },
-                          trailing: _device.address == d.address
-                              ? const Icon(
-                                  Icons.check,
-                                  color: Colors.green,
-                                )
-                              : null,
+                          trailing:
+                              _device != null && _device!.address == d.address
+                                  ? const Icon(
+                                      Icons.check,
+                                      color: Colors.green,
+                                    )
+                                  : null,
                         ))
                     .toList(),
               ),
-            ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _scanForBluetoothDevices,
+        onPressed: _scanning ? null : _scanForBluetoothDevices,
         tooltip: 'Scan',
         child: const Icon(Icons.scanner),
       ),
